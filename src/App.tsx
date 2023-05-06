746379ba2445573d700f8e0b75e88169c11d0e47
ljimshitashvili <jimshitashvili4444@gmail.com>
2023-05-06 15:06:53 +0400

    Intro text add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 import { GlobalStyle } from "./GlobalStyles";
 
+import bgMob from "./assets/bg-intro-mobile.png";
+
+import TextInfo from "./TextIntro";
+
 function App() {
   return (
     <Container>
       <GlobalStyle />
+      <TextInfo />
     </Container>
   );
 }
@@ -14,5 +19,17 @@ export default App;
 const Container = styled.div`
   width: 100%;
   min-height: 100vh;
-  background-color: black;
+  background: #ff7979;
+  background-image: url(${bgMob});
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 64px;
+  justify-content: center;
+
+  @media (min-width: 1024px) {
+    flex-direction: row;
+    gap: 45px;
+  }
 `;
---
+++ src/TextIntro.tsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export default function TextInfo() {
+  return (
+    <Container>
+      <h1>Learn to code by watching others</h1>
+      <h2>
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers think is
+        invaluable.{" "}
+      </h2>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  align-items: center;
+  max-width: 327px;
+
+  h1 {
+    font-weight: 700;
+    font-size: 28px;
+    line-height: 36px;
+    text-align: center;
+    letter-spacing: -0.291667px;
+    color: #ffffff;
+  }
+
+  h2 {
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 26px;
+    text-align: center;
+    color: #ffffff;
+  }
+
+  @media (min-width: 1024px) {
+    gap: 32px;
+    max-width: 525px;
+
+    h1 {
+      font-size: 50px;
+      line-height: 55px;
+      text-align: left;
+      letter-spacing: -0.520833px;
+    }
+
+    h2 {
+      text-align: left;
+    }
+  }
+`;
